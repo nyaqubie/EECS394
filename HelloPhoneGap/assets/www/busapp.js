2522ecef6e9b951e03ad7ed722d9d53f3784383c
Matt Schuchhardt <shook@shook-VirtualBox.(none)>
2011-10-26 21:50:30 -0500

    Added comments

--- HelloPhoneGap/assets/www/busapp.js
+++ HelloPhoneGap/assets/www/busapp.js
@@ -1,5 +1,6 @@
 function getStops(){
-	//This is a placeholder until we have the actual functionality to get the stops
+	//This is a placeholder until we have the actual functionality to get the stops from the CSV file
+	//It will eventually just return a list of the stops
 	var stops = new Array();
 	stops[0] = "";
 	stops[1] = "Stop1";
@@ -8,6 +9,8 @@ function getStops(){
 }
 
 function listStopsinOptionsFormat(){
+	//This function takes the stops in the format from getStops() and prints them out as a list of
+	//options tags (for the drop-down menu)
 	stops = getStops();
 	for (i in stops){
 		document.write("<option value=" + i + ">");
@@ -17,17 +20,20 @@ function listStopsinOptionsFormat(){
 }
 
 function selectedRedirect(thingy){
+	//This function redirects the app to the showstop page, and appends the stop ID
+	//This is run when the dropdown menu changes
 	window.location = 'showstop.html?stop=' + thingy.value;
 }
 
 function getQueryVariable(variable) {
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i=0;i<vars.length;i++) {
-    var pair = vars[i].split("=");
-    if (pair[0] == variable) {
-      return pair[1];
-    }
-  } 
-  alert('Query Variable ' + variable + ' not found');
+	//This function analyzes the URL and pulls out the specified variable (POST-style)
+	var query = window.location.search.substring(1);
+	var vars = query.split("&");
+	for (var i=0;i<vars.length;i++) {
+		var pair = vars[i].split("=");
+		if (pair[0] == variable) {
+			return pair[1];
+		}
+	} 
+	alert('Query Variable ' + variable + ' not found');
 }

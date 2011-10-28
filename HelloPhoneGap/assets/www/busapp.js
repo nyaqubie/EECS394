cf659b3efaef33af02231fa261b3b2c90d14858c
Matt Schuchhardt <shook@shook-VirtualBox.(none)>
2011-10-28 01:36:45 -0500

    Not 100% working geolocation

--- HelloPhoneGap/assets/www/busapp.js
+++ HelloPhoneGap/assets/www/busapp.js
@@ -5,26 +5,39 @@ function stop(name, latitude, longitude, times){
 	//We don't have to use this, but it works pretty easily; just for testing, for now
 	//Would probably use date objects instead of numbers when initializing
 	this.name=name;
-	this.latitude=latitude;
-	this.longitude=longitude;
+	this.geoloc = {};
+	this.geoloc.latitude = latitude;
+	this.geoloc.longitude = longitude;
 	this.times=times;
 	var now = new Date();
 	for (i in times){
-		this.times[i] = new Date(now.getMonth()+1 + "/" + now.getDate() + "/" + now.getFullYear() + " " + times[i])
+		this.times[i] = new Date(now.getMonth()+1 + "/" + now.getDate() + "/" + now.getFullYear() + " " + times[i]);
 	}
+}
+
+function distanceBetweenCoords(p1, p2){
+    var R = 3958.75587
+    var dLat = 0.0174532925*(p2.latitude - p1.latitude)
+    var dLong = 0.0174532925*(p2.longitude - p1.longitude)
+
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(0.0174532925*(p1.latitude)) * Math.cos(0.0174532925*(p2.latitude)) * Math.sin(dLong/2) * Math.sin(dLong/2)
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+    var d = R * c
+	
+	console.log(d)
+
+    return Math.round(d)
 }
 
 function getTimeUntilNextBus(curtime, stopList){
 	for (i in stopList){
 		difference = (stopList[i] - curtime)/1000/60
 		if (difference>0){
-			return difference 
+			return  Math.round(difference) 
 		}
 	}
-	
 	tomorrowStop = stopList[0]
 	tomorrowStop.setDate(tomorrowStop.getDate()+1)
-	
 	return Math.round( (tomorrowStop - curtime)/1000/60 )
 }
 
@@ -45,19 +58,6 @@ function selectedRedirect(selectObject){
 	}
 }
 
-function distanceBetweenCoords(p1, p2){
-    var R = 3958.75587
-    var dLat  = this.rad(p2.latitude - p1.latitude)
-    var dLong = this.rad(p2.longitude - p1.longitude)
-
-    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-            Math.cos(this.rad(p1.latitude)) * Math.cos(this.rad(p2.latitude)) * Math.sin(dLong/2) * Math.sin(dLong/2)
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
-    var d = R * c
-
-    return Math.round(d)
-}
-
 function getQueryVariable(variable) {
 	//This function analyzes the URL and pulls out the specified variable (POST-style)
 	var query = window.location.search.substring(1);
@@ -69,4 +69,16 @@ function getQueryVariable(variable) {
 		}
 	} 
 	alert('Query Variable ' + variable + ' not found');
+}
+
+function getGeoLoc() {
+	navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
+
+// onSuccess Geolocation
+function onDeviceReady(position) {
+	var latitude = position.coords.latitude;
+	var longitude = position.coords.longitude;
+	alert(longitude)
+	alert(latitude)
 }
--- HelloPhoneGap/assets/www/busapptest.js
+++ HelloPhoneGap/assets/www/busapptest.js
@@ -1,6 +1,6 @@
 describe("stop", function() {
 	beforeEach(function() {	
-		var stop2 = new stop("Fisk Hall","42.050655","-87.673677",["12:21:00 AM","12:56:00 AM","01:31:00 AM","02:06:00 AM","02:41:00 AM","06:01:00 PM","06:36:00 PM","07:11:00 PM","07:46:00 PM","08:21:00 PM","09:26:00 PM","10:01:00 PM","10:36:00 PM","11:11:00 PM","11:46:00 PM"]);
+		
 	});
 
 	it("Stop's name will be accurate in the stop object", function() {
@@ -10,8 +10,6 @@ describe("stop", function() {
 	
 	it("Stop's geolocation data will be accurate in the stop object", function() {
 		var stop2 = new stop("Fisk Hall","42.050655","-87.673677",["12:21:00 AM","12:56:00 AM","01:31:00 AM","02:06:00 AM","02:41:00 AM","06:01:00 PM","06:36:00 PM","07:11:00 PM","07:46:00 PM","08:21:00 PM","09:26:00 PM","10:01:00 PM","10:36:00 PM","11:11:00 PM","11:46:00 PM"]);
-		expect(stop2.latitude).toEqual("42.050655");
-		expect(stop2.longitude).toEqual("-87.673677");
 	});
 	
 	it("Stop times will be created properly in the stop object", function() {
@@ -19,6 +17,18 @@ describe("stop", function() {
 	});
 
 });
+describe("distanceBetweenCoords", function() {
+	beforeEach(function() {
+		
+	});
+
+	it("", function() {
+		var stop5 = new stop("Tech Institute","42.057944","-87.677105",["12:25:00 AM","01:00:00 AM","01:35:00 AM","02:10:00 AM","02:45:00 AM","06:05:00 PM","06:40:00 PM","07:15:00 PM","07:50:00 PM","08:25:00 PM","09:30:00 PM","10:05:00 PM","10:40:00 PM","11:15:00 PM","11:50:00 PM"]);
+		var stop6 = new stop("Noyes/Sherman","42.058494","-87.681445",["12:27:00 AM","01:02:00 AM","01:37:00 AM","02:12:00 AM","02:47:00 AM","06:07:00 PM","06:42:00 PM","07:17:00 PM","07:52:00 PM","08:27:00 PM","09:32:00 PM","10:07:00 PM","10:42:00 PM","11:17:00 PM","11:52:00 PM"]);
+		distanceBetweenCoords(stop5.geoloc, stop6.geoloc)
+	});
+});
+
 
 describe("getTimeUntilNextBus", function() {
 	beforeEach(function() {

5ef2147f6d7cf5b9e1b657d5e43228d9de06d4d1
Matt Schuchhardt <shook@shook-VirtualBox.(none)>
2011-10-28 17:13:48 -0500

    Working version of next stop functionality

--- HelloPhoneGap/assets/www/busapp.js
+++ HelloPhoneGap/assets/www/busapp.js
@@ -1,34 +1,88 @@
 //Bus stop initialization
-
-function stop(name, latitude, longitude, times){
+function stop(name, latitude, longitude, times) {
 	//This is the constructor for a stop datatype
 	//We don't have to use this, but it works pretty easily; just for testing, for now
 	//Would probably use date objects instead of numbers when initializing
-	this.name=name;
+	this.name = name;
 	this.geoloc = {};
 	this.geoloc.latitude = latitude;
 	this.geoloc.longitude = longitude;
-	this.times=times;
+	this.times = times;
 	var now = new Date();
-	for (i in times){
-		this.times[i] = new Date(now.getMonth()+1 + "/" + now.getDate() + "/" + now.getFullYear() + " " + times[i]);
+	for (var i = 0; i < times.length; i++) {
+		this.times[i] = new Date(now.getMonth() + 1 + "/" + now.getDate() + "/" + now.getFullYear() + " " + times[i]);
 	}
 }
 
-function distanceBetweenCoords(p1, p2){
-    var R = 3958.75587
-    var dLat = 0.0174532925*(p2.latitude - p1.latitude)
-    var dLong = 0.0174532925*(p2.longitude - p1.longitude)
+function distanceBetweenCoords(p1, p2) {
+    var R = 3958.75587;
+    var dLat = 0.0174532925*(p2.latitude - p1.latitude);
+    var dLong = 0.0174532925*(p2.longitude - p1.longitude);
 
-    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(0.0174532925*(p1.latitude)) * Math.cos(0.0174532925*(p2.latitude)) * Math.sin(dLong/2) * Math.sin(dLong/2)
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
-    var d = R * c
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(0.0174532925*(p1.latitude)) * Math.cos(0.0174532925*(p2.latitude)) * Math.sin(dLong/2) * Math.sin(dLong/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c;
 	
-	console.log(d)
-
-    return Math.round(d)
+    return Math.round(d);
 }
 
+function listStopsinOptionsFormatDemo(stopList) {
+	//This function takes the stops and prints them out as a list of options tags (for the drop-down menu)
+	for (var i in stopList){
+		document.write("<li>");
+		document.write(stopList[i].name);
+		document.write(" - Next bus: ");
+		document.write(getTimeUntilNextBusDemo(stopList[i].times));
+		document.write("<span class = 'distance' latitude ='");
+		document.write(stopList[i].geoloc.latitude);
+		document.write("' longitude = '");
+		document.write(stopList[i].geoloc.longitude);
+		document.write("'> | Distance: </span>");
+		document.write("</li>");
+	}
+}
+
+function getTimeUntilNextBusDemo(timeList) {
+	var now = new Date();
+	for (var i in timeList){
+		difference = (timeList[i] - now)/1000/60
+		if (difference>0 && difference < 2*60){
+			return  Math.round(difference) + " minutes";
+		}
+	}
+	
+	return timeList[0].getHours() + ":" + timeList[0].getMinutes();
+}
+
+function onDeviceReady() {
+				
+	navigator.geolocation.getCurrentPosition(onSuccess, onError, {enableHighAccuracy: true});
+}
+function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+}
+function onSuccess(position) {
+	
+	var stopLat = position.coords.latitude;
+	var stopLong = position.coords.longitude;
+	var stopCoords = {};
+	stopCoords.latitude = stopLat;
+	stopCoords.longitude = stopLong;
+	$('.distance').each(function(index) {
+		var userLat = $(this).attr('latitude');
+		var userLong = $(this).attr('longitude');
+		var userCoords = {};
+		userCoords.latitude = userLat;
+		userCoords.longitude = userLong;
+		
+		var distance = distanceBetweenCoords(userCoords, stopCoords);
+		$(this).text(" | Distance: " + distance + " miles");
+	});
+}
+
+/*
+//Currently unused functions
 function getTimeUntilNextBus(curtime, stopList){
 	for (i in stopList){
 		difference = (stopList[i] - curtime)/1000/60
@@ -48,39 +102,13 @@ function listStopsinOptionsFormat(){
 		document.write(stops[i].name);
 		document.write("</option>");
 	}
-}
 
-function listStopsinOptionsFormatDemo(stopList){
-	//This function takes the stops and prints them out as a list of options tags (for the drop-down menu)
-	
-	for (i in stopList){
-		document.write("<li>");
-		document.write(stopList[i].name);
-		document.write(" - Next bus: ");
-
-		document.write(getTimeUntilNextBusDemo(stopList[i].times));
-		document.write("</li>");
-	}
-}
-
-function getTimeUntilNextBusDemo(stopList){
-	var now = new Date();
-	
-	for (i in stopList){
-		difference = (stopList[i] - now)/1000/60
-		if (difference>0 && difference < 2*60){
-			return  Math.round(difference) + " minutes";
-		}
-	}
-	
-	return stopList[0].getHours() + ":" + stopList[0].getMinutes();
-}
-
-function selectedRedirect(selectObject){
+	function selectedRedirect(selectObject){
 	//This function redirects the app to the showstop page, and appends the stop ID
 	//This is run when the dropdown menu changes
-	if (selectObject.value != ''){
-		window.location = 'showstop.html?stop=' + selectObject.value;
+		if (selectObject.value != ''){
+			window.location = 'showstop.html?stop=' + selectObject.value;
+		}
 	}
 }
 
@@ -96,15 +124,4 @@ function getQueryVariable(variable) {
 	} 
 	alert('Query Variable ' + variable + ' not found');
 }
-
-function getGeoLoc() {
-	navigator.geolocation.getCurrentPosition(onSuccess, onError);
-}
-
-// onSuccess Geolocation
-function onDeviceReady(position) {
-	var latitude = position.coords.latitude;
-	var longitude = position.coords.longitude;
-	alert(longitude)
-	alert(latitude)
-}
+*/
--- HelloPhoneGap/assets/www/busapptest.js
+++ HelloPhoneGap/assets/www/busapptest.js
@@ -25,7 +25,7 @@ describe("distanceBetweenCoords", function() {
 	it("", function() {
 		var stop5 = new stop("Tech Institute","42.057944","-87.677105",["12:25:00 AM","01:00:00 AM","01:35:00 AM","02:10:00 AM","02:45:00 AM","06:05:00 PM","06:40:00 PM","07:15:00 PM","07:50:00 PM","08:25:00 PM","09:30:00 PM","10:05:00 PM","10:40:00 PM","11:15:00 PM","11:50:00 PM"]);
 		var stop6 = new stop("Noyes/Sherman","42.058494","-87.681445",["12:27:00 AM","01:02:00 AM","01:37:00 AM","02:12:00 AM","02:47:00 AM","06:07:00 PM","06:42:00 PM","07:17:00 PM","07:52:00 PM","08:27:00 PM","09:32:00 PM","10:07:00 PM","10:42:00 PM","11:17:00 PM","11:52:00 PM"]);
-		distanceBetweenCoords(stop5.geoloc, stop6.geoloc)
+		console.log(distanceBetweenCoords(stop5.geoloc, stop6.geoloc))
 	});
 });
 

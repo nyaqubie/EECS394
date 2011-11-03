2860970a78bcf34c13ff3541f34e8dd880a519e4
Matt Schuchhardt <shook@shook-VirtualBox.(none)>
2011-11-03 15:00:34 -0500

    Minor changes, created event file, now asks for the number of intersted people from the server

---
+++ MPD/assets/www/app.js
@@ -0,0 +1,12 @@
+function getQueryVariable(variable) {
+	//This function analyzes the URL and pulls out the specified variable (POST-style)
+	var query = window.location.search.substring(1);
+	var vars = query.split("&");
+	for (var i=0;i<vars.length;i++) {
+		var pair = vars[i].split("=");
+		if (pair[0] == variable) {
+			return pair[1];
+		}
+	} 
+	alert('Query Variable ' + variable + ' not found');
+}

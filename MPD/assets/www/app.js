ce3437d51865b357d8f4796fe5902424bc855ae3
Matt Schuchhardt <shook@shook-VirtualBox.(none)>
2011-11-14 11:40:55 -0600

    Refactored javascript locations

--- MPD/assets/www/app.js
+++ MPD/assets/www/app.js
@@ -10,3 +10,122 @@ function getQueryVariable(variable) {
 	} 
 	alert('Query Variable ' + variable + ' not found');
 }
+
+function commitToLocation(){
+	var eveid = getQueryVariable('eventid');
+	var locid = getQueryVariable('locationid');
+	var devuuid = device.uuid;
+	$.ajax({
+		url: 'http://69.164.198.224/showinterest',
+		type: 'GET',
+		dataType: 'jsonp',
+		data:{uuid: devuuid, locationid: locid, eventid:eveid},
+		jsonp: 'jsoncallback',
+		timeout: 10000,
+		success: function(data, status){
+			$('#commitlink').remove();
+			var link = document.createElement('a');
+			link.setAttribute('rel','external');
+			link.setAttribute('data-role','button');
+			link.appendChild(document.createTextNode('You are interested in this location'));
+			$("#footer").before(link);	//add the link before the footer
+			$("html").trigger('create')	//needed to apply jqmobile style changes on dynamic content
+		},
+		error: function(){
+			alert('bad')
+		}
+	});
+}
+
+function createBackButton(){
+	var eveid = getQueryVariable('eventid');
+	var locid = getQueryVariable('locationid');
+	var backlink = document.createElement('a');
+	var devuuid = device.uuid;
+	backlink.setAttribute('href','event.html?eventid=' + eveid);
+	backlink.setAttribute('rel','external');
+	backlink.setAttribute('data-role','button');
+	backlink.appendChild(document.createTextNode('Back'));
+	$("#events").after(backlink);
+}
+
+function createInterestButton(){
+	var devuuid = device.uuid;
+	var eveid = getQueryVariable('eventid');
+	var locid = getQueryVariable('locationid');
+	$.ajax({
+		url: 'http://69.164.198.224/checkinterest',
+		type: 'GET',
+		dataType: 'jsonp',
+		data:{uuid: devuuid, locationid: locid, eventid:eveid},
+		jsonp: 'jsoncallback',
+		timeout: 10000,
+		success: function(data, status){
+			var link = document.createElement('a');
+			link.setAttribute('rel','external');
+			link.setAttribute('data-role','button');
+			link.setAttribute('id','commitlink');
+			if (data == 0){
+				link.setAttribute('onclick','commitToLocation()');
+				link.appendChild(document.createTextNode('Show interest in this location'));
+			}
+			else{
+				link.appendChild(document.createTextNode('You are interested in this location'));
+			}
+			$("#footer").before(link);	//add the link before the footer
+			$("html").trigger('create')	//needed to apply jqmobile style changes on dynamic content
+		},
+		error: function(){
+			alert('bad')
+		}
+	});
+}
+
+function listLocationsForEvent(){
+	var eventid = getQueryVariable('eventid');
+	$.ajax({
+		url: 'http://69.164.198.224/getlocations',
+		type: 'GET',
+		dataType: 'jsonp',
+		jsonp: 'jsoncallback',
+		timeout: 1000,
+		success: function(data, status){
+			for (var i = 0; i<data.length; i++){
+				var link = document.createElement('a');
+				link.setAttribute('href','location.html?eventid=' + eventid + '&locationid=' + data[i][0]);
+				link.setAttribute('rel','external');
+				link.setAttribute('data-role','button');
+				link.appendChild(document.createTextNode(data[i][1] + ' - ' + data[i][2]));
+				$("#footer").before(link);	//add the link before the footer
+				$("html").trigger('create')	//needed to apply jqmobile style changes on dynamic content
+			}
+		},
+		error: function(){
+			alert('bad')
+		}
+	});
+}
+
+function getEvents(){
+	$.ajax({
+		url: 'http://69.164.198.224/getevents',
+		type: 'GET',
+		dataType: 'jsonp',
+		jsonp: 'jsoncallback',
+		timeout: 10000,
+		success: function(data, status){
+			for (var i = 0; i<data.length; i++){
+				var link = document.createElement('a');
+				link.setAttribute('href','event.html?eventid='+data[i][0]);
+				link.setAttribute('rel','external');
+				link.setAttribute('data-role','button');
+				link.appendChild(document.createTextNode(data[i][1] + " " + data[i][2]));
+				$("#footer").before(link);	//add the link before the footer
+				$("html").trigger('create')	//needed to apply jqmobile style changes on dynamic content
+			}
+		},
+		error: function(){
+			alert('bad')
+		}
+	});
+}

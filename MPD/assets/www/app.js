e19de7fe13b02bb6f155c995a6a5ff3143855cf4
Matt Schuchhardt <shook@shook-VirtualBox.(none)>
2011-11-16 18:41:33 -0600

    Distance stuff

--- MPD/assets/www/app.js
+++ MPD/assets/www/app.js
@@ -93,7 +93,6 @@ function showLocationInfo(){
 		success: function(data, status){
 			var name = data[0][0];
 			var address = data[0][1];
-			console.log(name);
 			
 			var header = document.createElement('h2');
 			header.appendChild(document.createTextNode(name));
@@ -109,7 +108,7 @@ function showLocationInfo(){
 	});
 }
 
-function listLocationsForEvent(){
+function listLocationsForEvent(userCoords){
 	var eventid = getQueryVariable('eventid');
 	$.ajax({
 		url: 'http://69.164.198.224/getlocations',
@@ -126,10 +125,15 @@ function listLocationsForEvent(){
 				var geolocation = data[i][3];
 				var numinterested = data[i][4];
 				
+				var locationCoords = {};
+				locationCoords.latitude = geolocation.split(',')[0];
+				locationCoords.longitude = geolocation.split(',')[1];
+				var distance = distanceBetweenCoords(locationCoords, userCoords);
+				
 				link.setAttribute('href','location.html?eventid=' + eventid + '&locationid=' + data[i][0]);
 				link.setAttribute('rel','external');
 				link.setAttribute('data-role','button');
-				link.appendChild(document.createTextNode(name + ' ' + numinterested));
+				link.appendChild(document.createTextNode(name + ' ' + distance + 'mi, ' + numinterested + ' interested' ));
 				$("#footer").before(link);	//add the link before the footer
 				$("html").trigger('create')	//needed to apply jqmobile style changes on dynamic content
 			}
@@ -162,4 +166,33 @@ function getEvents(){
 			alert('bad')
 		}
 	});
+}
+
+function distanceBetweenCoords(p1, p2) {
+    var R = 3958.75587;
+    var dLat = 0.0174532925*(p2.latitude - p1.latitude);
+    var dLong = 0.0174532925*(p2.longitude - p1.longitude);
+
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(0.0174532925*(p1.latitude)) * Math.cos(0.0174532925*(p2.latitude)) * Math.sin(dLong/2) * Math.sin(dLong/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c;
+	
+    return Math.round(d);
+}
+
+function getGPSLocation() {		
+	navigator.geolocation.getCurrentPosition(GPSSuccess, GPSError, {enableHighAccuracy: true});
+}
+function GPSError(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+}
+function GPSSuccess(position) {
+	var userLat = position.coords.latitude;
+	var userLong = position.coords.longitude;
+	var userCoords = {};
+	userCoords.latitude = userLat;
+	userCoords.longitude = userLong;
+	listLocationsForEvent(userCoords)
+	//var distance = distanceBetweenCoords(userCoords, stopCoords);
 }

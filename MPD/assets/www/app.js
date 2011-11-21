5c854a92018066c05e830272b5bf9acdedc9e18f
Matt Schuchhardt <shook@shook-VirtualBox.(none)>
2011-11-21 14:07:27 -0600

    Removed javascript distance calc function

--- MPD/assets/www/app.js
+++ MPD/assets/www/app.js
@@ -38,16 +38,12 @@ function getEvents(){
 
 //List buttons for all of the locations for an event
 function listLocationsForEvent(userCoords){
-	console.log(userCoords)
 	var eventid = getQueryVariable('eventid');
 	var url='http://69.164.198.224/getlocationsinradius';
 	var data={eventid: eventid, radius: 1, geolocation: userCoords};
 
 	var funct = function(data, status){
 		for (var i = 0; i<data.length; i++){
-		
-			console.log(data[i])
-		
 			var name = data[i][1];
 			var address = data[i][2];
 			var numinterested = data[i][3];
@@ -166,7 +162,6 @@ function GPSSuccess(position) {
 	listLocationsForEvent(position.coords.latitude + ',' + position.coords.longitude);
 }
 
-
 ///////////////////////////////////////////////////////////////////////////////
 //Data Manipulation or Display Functions
 ///////////////////////////////////////////////////////////////////////////////
@@ -193,17 +188,4 @@ function getQueryVariable(variable) {
 		}
 	} 
 	alert('Query Variable ' + variable + ' not found');
-}
-
-//Calculate the distance between two coordinates
-function distanceBetweenCoords(p1, p2) {
-    var R = 3958.75587;
-    var dLat = 0.0174532925*(p2.latitude - p1.latitude);
-    var dLong = 0.0174532925*(p2.longitude - p1.longitude);
-
-    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(0.0174532925*(p1.latitude)) * Math.cos(0.0174532925*(p2.latitude)) * Math.sin(dLong/2) * Math.sin(dLong/2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    var d = R * c;
-	
-    return Math.round(d);
 }

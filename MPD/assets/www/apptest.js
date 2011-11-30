d71b1bbc6d1edd45c381ed5ceb518992efe457fb
Matt Schuchhardt <shook@shook-VirtualBox.(none)>
2011-11-30 16:27:03 -0600

    Back button test for location page

--- MPD/assets/www/apptest.js
+++ MPD/assets/www/apptest.js
@@ -1,6 +1,6 @@
 jasmine.getFixtures().fixturesPath = '.';
 
- describe("index.html", function() {
+describe("index.html", function() {
 
  	it("should have a visible banner initially, and no visible banner after the 5 seconds", function() {
 		var fixtureUrl = "index.html";
@@ -44,61 +44,36 @@ describe("events.html", function() {
 		});
 	});
 
-});
+}); 
 
-/* 	beforeEach(function() {	
+describe("locations.html", function() {
+
+	it("should .....", function() {
+		var fixtureUrl = "locations.html";
+		jasmine.getFixtures().load(fixtureUrl);
+		waits(100)
+
+		runs( function () {
+		});
+	});
+
+}); 
+
+describe("location.html", function() {
+	it("should create the correct back button", function() {
+		var fixtureUrl = "location.html";
+		jasmine.getFixtures().load(fixtureUrl);
+		waits(100)
+		
 		var altQuery = function(){
 			return '11';
 		}
-		spyOn(window, "getQueryVariable").andCallFake(altQuery);
 
-		var testarea = document.createElement('div');
-		testarea.setAttribute('id','testarea');
-		$('html').append(testarea);
-		
-		var backlink = document.createElement('h1')
-		backlink.setAttribute('id','events');
-		$('#testarea').append(backlink)
+		runs( function () {
+			spyOn(window, "getQueryVariable").andCallFake(altQuery);
+			createBackButton()
+			expect($('#back').attr('href')).toEqual('locations.html?eventid=11');
+			console.log($('#jasmine-fixtures'))
+		});
 	});
-	
-	it("should create a button as a sibling of 'h1#events' with the proper href attribute", function() {
-		createBackButton()	
-		expect($('#events').next().attr('href')).toEqual('locations.html?eventid=11')
-	});
-	
-	afterEach(function() {
-		$('#testarea').remove()
-
-
-/*describe("distanceBetweenCoords tests", function(){
-	it("should return '3265' when geocoords '12,14' and '-32,-4' are given", function() {
-		var locationCoords1 = {};
-		locationCoords1.latitude = '12';
-		locationCoords1.longitude = '14';
-		
-		var locationCoords2 = {};
-		locationCoords2.latitude = '-32';
-		locationCoords2.longitude = '-4';
-		
-		distance = distanceBetweenCoords(locationCoords1, locationCoords2)
-		expect(distance).toEqual(3265)
-	});
-});*/
-
-/*describe("getEvents tests", function(){
-	beforeEach(function() {	
-		var testarea = document.createElement('div');
-		testarea.setAttribute('id','testarea');
-		$('html').append(testarea);
-	});
-	
-	it("should return the proper set of events as buttons", function() {
-		console.log(getEvents)
-	});
-	
-	afterEach(function() {
-		$('#testarea').remove()
-	});
-	//getEvents()
-	
-});*/
+});
